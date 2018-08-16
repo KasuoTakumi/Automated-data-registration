e8d0c10ab10bfbca7ecb6b6d7b8414ba7cbceb8b
Kso <kasuo.takumi@gmail.com>
2018-08-16 23:17:23 +0900

    add functions.php　関数群

---
+++ dev/grass_tool/functions.php
@@ -0,0 +1,17 @@
+<?php
+
+function generater_product_name(){
+
+}
+
+function generater_product_category(){
+
+}
+
+function generater_product_price(){
+
+}
+
+function generater_product_detail(){
+
+}
