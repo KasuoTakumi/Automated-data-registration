90dd899d2700f0acd94fbc3ecb5d5856be454dc4
Kso <kasuo.takumi@gmail.com>
2018-08-18 21:48:42 +0900

    ;)

--- dev/grass_tool/index.php
+++ dev/grass_tool/index.php
@@ -4,24 +4,20 @@ include_once('./functions.php');
 
 $html = file_get_contents('./index.html');
 
-//登録させる件数
 @$add_count = $_POST['max_value'];
-$product_name_array = array();
-$f = fopen("./csv/data.csv", "r");
-$i = 0;
-while($line = fgetcsv($f)){
-    array_push($product_name_array,$line);
-    $i +=1;
-}
-// test.csvを閉じます。
-fclose($f);
 
-var_dump($product_name_array);
 //登録させる件数が正しく入力されている場合には登録処理を実行させる
 if(isset($add_count)){
+    echo'true';
     if($add_count>0){
-        echo'hello';
+        echo'true';
+        generat_demo_data($add_count);
+        $add_count = '';
+    }else{
+        echo'false';
     }
+}else{
+    echo'false';
 }
 
 echo $html;
